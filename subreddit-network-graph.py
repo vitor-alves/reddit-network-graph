4f637998905561a80397681a3554d5319b6c1eab
vitor <vitoralves842@gmail.com>
2017-08-26 17:00:16 -0300

    working

--- subreddit-network-graph.py
+++ subreddit-network-graph.py
@@ -3,11 +3,15 @@ import requests.auth
 import re
 import time
 import sys
+import random
+import csv
+
+start_time = time.time()
 
 # OAuth info
-client_id=sys.argv[3]
-secret=sys.argv[4]
-access_token=''
+client_id = sys.argv[3]
+secret = sys.argv[4]
+access_token = ''
 
 # Request a token
 client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
@@ -17,39 +21,48 @@ response = requests.post("https://www.reddit.com/api/v1/access_token", auth=clie
 access_token=response.json()['access_token']
 print(response.json())
 
+output = open('search_output.csv', 'w')
 past_subreddits = []
-def search_subreddits(subreddit, counter):
-    past_subreddits.append(subreddit.lower())
+subreddits_list = ['buddhism', 'opensource', 'brasil']
+for i in range(0, 100000000):
+    # Get a random subreddit name from subreddits_list
+    subreddit = str(random.choice(subreddits_list))
+    subreddits_list.remove(subreddit)
+    if(subreddit in past_subreddits):
+        continue
+    past_subreddits.append(subreddit)
+
+    # Get related subreddits from sidebar
     headers = {"Authorization": "bearer "+access_token, "User-Agent": "redditNetworkGraph/0.1 by standard_thrownaway"}
     response = requests.get("https://oauth.reddit.com/r/"+subreddit+"/about/.json", headers=headers)
-
     if(response.status_code != 200):
         try:
             print(response.json()['reason'])
         except:
             pass
         print("Code != 200. subreddit:" +subreddit)
-        return;
+        continue;
 
-    # give up (return) if not able to get description and subscribers
+    # Skip if not able to get description and subscribers
     try:
         subreddit_description = response.json()['data']['description']
         subreddit_subscribers = response.json()['data']['subscribers']
     except:
-        return
+        continue
 
     if(subreddit_subscribers < 10000):
-        return
+        continue
 
     subreddits_regex = re.compile(r'r[\/](\w+)')
-    subreddits_list = subreddits_regex.findall(subreddit_description)
+    related_subreddits = subreddits_regex.findall(subreddit_description)
+    related_subreddits =  [x.lower() for x in related_subreddits]
+    related_subreddits = list(set(related_subreddits)) #remove duplicates
+    subreddits_list.extend(related_subreddits)
     subreddits_list = list(set(subreddits_list)) #remove duplicates
-   
-    print(subreddit)
-    counter = counter -1
-    for i in subreddits_list:
-        if(i.lower() not in past_subreddits and counter > 0):
-            time.sleep(0) # to respect API request limits 
-            search_subreddits(i, counter)
+    print(subreddit + ' / ' + 'past_subreddits_size: '+str(len(past_subreddits))  + ' / ' +  'subreddits_list_size: '+str(len(subreddits_list))  + ' / ' +  'running_time: '+str(round(time.time() - start_time, 1)))
     
-search_subreddits('Games', 1000)
+    output.write(subreddit + ',' + str(subreddit_subscribers) + ',' + ",".join(related_subreddits) + '\n')
+    output.flush()
+
+output.close()
+

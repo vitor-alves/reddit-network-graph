a52dcb1a6c7e486fcf9957779fb5cc222db6836a
vitor <vitoralves842@gmail.com>
2017-08-27 21:57:13 -0300

    can now output edges and nodes files

---
+++ mapper.py
@@ -0,0 +1,49 @@
+import csv
+
+csv_input = open('search_output_completo.csv', 'r')
+csv_reader = csv.reader(csv_input)
+nodes_list = []
+edge_list = []
+for row in csv_reader:
+	nodes_list.append(row)
+
+def generate_nodes_file():
+	output = open('nodes.csv', 'w')
+	output.write('Id,Label,Size'+'\n')
+	for node in nodes_list:
+		size = str(int(float(node[1])/1000))
+		size = str( ((int(node[1])-10000) * (1000-200))/(18480000-10000) + 200)
+		output.write(node[0]+','+node[0]+','+ size+'\n')
+
+def generate_edges_file():
+	output = open('edges.csv', 'w')
+	output.write('Source,Target,Type'+'\n')
+	for edge in edge_list:
+		output.write(edge[0]+','+edge[1]+','+'Undirected'+'\n')
+
+def generate_edges():
+	for node in nodes_list:
+		for related_node in node[2:]:
+			if(len(related_node) > 0):
+				edge_list.append([node[0], related_node])
+
+# huehuehue too dumb for production. fix this. Time complexity too fucking high 
+def remove_mutual_edges(): 
+	for i,edge in enumerate(edge_list):
+		for j,edge_temp in enumerate(edge_list[i+1:]):
+			if(set(edge) == set(edge_temp)):
+				print('edge: '+str(edge) + " / " +'edge_temp: '+str(edge_temp) + ' / '+str(i) + ' / ' +str(j))
+				edge_list.pop(j)
+
+# mix with remove_mutual_edges maybe?
+def remove_self_loop_edges():
+	for edge in edge_list:
+		if(edge[0] == edge[1]):
+			print('edge1: '+str(edge[0]) + " / " +'edge_2: '+str(edge[1]))
+			edge_list.remove(edge)
+
+generate_edges()
+#remove_self_loop_edges()
+#remove_mutual_edges()
+generate_nodes_file()
+generate_edges_file()
--- subreddit-network-graph.py
+++ subreddit-network-graph.py
@@ -5,6 +5,7 @@ import time
 import sys
 import random
 import csv
+import threading
 
 start_time = time.time()
 
@@ -13,28 +14,46 @@ client_id = sys.argv[3]
 secret = sys.argv[4]
 access_token = ''
 
-# Request a token
-client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
-post_data = {"grant_type": "password", "username": sys.argv[1], "password": sys.argv[2]}
-headers = {"User-Agent": "redditNetworkGraph/0.1 by standard_thrownaway"}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-access_token=response.json()['access_token']
-print(response.json())
+def get_access_token():
+    threading.Timer(3500, get_access_token).start()
+    # Request a token
+    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password", "username": sys.argv[1], "password": sys.argv[2]}
+    headers = {"User-Agent": "redditNetworkGraph/0.1 by standard_thrownaway"}
+
+    try:
+        response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    except:
+        pass
+    while(response.status_code != 200):
+        try:
+            response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+        except:
+            pass
+    access_token=response.json()['access_token']
+    print(response.json())
+
+get_access_token()
 
 output = open('search_output.csv', 'w')
 past_subreddits = []
-subreddits_list = ['buddhism', 'opensource', 'brasil']
-for i in range(0, 100000000):
+subreddits_list = ['opensource']
+iterations = 0
+while(len(subreddits_list) != 0):
     # Get a random subreddit name from subreddits_list
     subreddit = str(random.choice(subreddits_list))
     subreddits_list.remove(subreddit)
     if(subreddit in past_subreddits):
         continue
     past_subreddits.append(subreddit)
+    past_subreddits = list(set(past_subreddits)) #remove duplicates
 
     # Get related subreddits from sidebar
     headers = {"Authorization": "bearer "+access_token, "User-Agent": "redditNetworkGraph/0.1 by standard_thrownaway"}
-    response = requests.get("https://oauth.reddit.com/r/"+subreddit+"/about/.json", headers=headers)
+    try:
+        response = requests.get("https://oauth.reddit.com/r/"+subreddit+"/about/.json", headers=headers)
+    except:
+        continue
     if(response.status_code != 200):
         try:
             print(response.json()['reason'])
@@ -59,10 +78,11 @@ for i in range(0, 100000000):
     related_subreddits = list(set(related_subreddits)) #remove duplicates
     subreddits_list.extend(related_subreddits)
     subreddits_list = list(set(subreddits_list)) #remove duplicates
-    print(subreddit + ' / ' + 'past_subreddits_size: '+str(len(past_subreddits))  + ' / ' +  'subreddits_list_size: '+str(len(subreddits_list))  + ' / ' +  'running_time: '+str(round(time.time() - start_time, 1)))
+    print(subreddit + ' / ' + 'past_subreddits_size: '+str(len(past_subreddits))  + ' / ' +  'subreddits_list_size: '+str(len(subreddits_list))  + ' / ' +  'running_time: '+str(round(time.time() - start_time, 1)) + ' / ' + 'iterations: '+str(iterations))
     
     output.write(subreddit + ',' + str(subreddit_subscribers) + ',' + ",".join(related_subreddits) + '\n')
     output.flush()
+    iterations = iterations+1
 
 output.close()
 
